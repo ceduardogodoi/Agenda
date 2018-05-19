e310cbff47b1529ef731b174df908c28cf3aa5a0
ceduardogodoi <carloseduardoalvesgodoi@hotmail.com>
2018-05-19 19:15:52 -0300

    Indentando o código

--- app/src/main/java/br/com/alura/agenda/ListaAlunosActivity.java
+++ app/src/main/java/br/com/alura/agenda/ListaAlunosActivity.java
@@ -1,7 +1,7 @@
 package br.com.alura.agenda;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class ListaAlunosActivity extends AppCompatActivity {
 
@@ -10,5 +10,5 @@ public class ListaAlunosActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_alunos);
     }
-
+    
 }
