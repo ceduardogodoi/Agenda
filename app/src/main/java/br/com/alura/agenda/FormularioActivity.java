a5d38cd0a36ac96706b284c341ba8733fb3514b0
ceduardogodoi <carloseduardoalvesgodoi@hotmail.com>
2018-05-21 22:36:49 -0300

    Aula 03 Exercício 02: Navegando entre telas

--- app/src/main/java/br/com/alura/agenda/FormularioActivity.java
+++ app/src/main/java/br/com/alura/agenda/FormularioActivity.java
@@ -1,8 +1,8 @@
 package br.com.alura.agenda;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
@@ -17,6 +17,8 @@ public class FormularioActivity extends AppCompatActivity {
 
         botaoSalvar.setOnClickListener(view -> {
             Toast.makeText(FormularioActivity.this, "Botão clicado", Toast.LENGTH_SHORT).show();
+
+            finish();
         });
     }
 
--- app/src/main/java/br/com/alura/agenda/ListaAlunosActivity.java
+++ app/src/main/java/br/com/alura/agenda/ListaAlunosActivity.java
@@ -1,8 +1,10 @@
 package br.com.alura.agenda;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 
 public class ListaAlunosActivity extends AppCompatActivity {
@@ -18,6 +20,12 @@ public class ListaAlunosActivity extends AppCompatActivity {
         ListView listaAlunos = findViewById(R.id.lista_alunos);
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, alunos);
         listaAlunos.setAdapter(adapter);
+
+        Button novoAluno = findViewById(R.id.novo_aluno);
+        novoAluno.setOnClickListener(view -> {
+            Intent intentVaiProFormulario = new Intent(ListaAlunosActivity.this, FormularioActivity.class);
+            startActivity(intentVaiProFormulario);
+        });
     }
 
 }
