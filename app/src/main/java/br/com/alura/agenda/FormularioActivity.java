1e9213b1f3738bab1e57919d8510d76d21e1a517
ceduardogodoi <carloseduardoalvesgodoi@hotmail.com>
2018-05-22 06:35:33 -0300

    Aula 03 Exercício 03: Utilizando menus

--- app/src/main/java/br/com/alura/agenda/FormularioActivity.java
+++ app/src/main/java/br/com/alura/agenda/FormularioActivity.java
@@ -2,7 +2,9 @@ package br.com.alura.agenda;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.Button;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.Toast;
 
 public class FormularioActivity extends AppCompatActivity {
@@ -11,14 +13,25 @@ public class FormularioActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario);
-
-        Button botaoSalvar = findViewById(R.id.formulario_salvar);
-
-        botaoSalvar.setOnClickListener(view -> {
-            Toast.makeText(FormularioActivity.this, "Botão clicado", Toast.LENGTH_SHORT).show();
-
-            finish();
-        });
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_formulario, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_formulario_ok:
+                Toast.makeText(FormularioActivity.this, "Botão clicado", Toast.LENGTH_SHORT).show();
+                finish();
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
