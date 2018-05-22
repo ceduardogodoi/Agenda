55986d0a2ebfaab74b7582964e0621f549723dca
ceduardogodoi <carloseduardoalvesgodoi@hotmail.com>
2018-05-22 06:52:59 -0300

    Ajuste: Removendo a referência ao MenuInflater e inflando diretamente do getMenuInflater()

--- app/src/main/java/br/com/alura/agenda/FormularioActivity.java
+++ app/src/main/java/br/com/alura/agenda/FormularioActivity.java
@@ -3,7 +3,6 @@ package br.com.alura.agenda;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.Toast;
 
@@ -17,8 +16,7 @@ public class FormularioActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu_formulario, menu);
+        getMenuInflater().inflate(R.menu.menu_formulario, menu);
 
         return super.onCreateOptionsMenu(menu);
     }
@@ -27,7 +25,7 @@ public class FormularioActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.menu_formulario_ok:
-                Toast.makeText(FormularioActivity.this, "Botão clicado", Toast.LENGTH_SHORT).show();
+                Toast.makeText(FormularioActivity.this, "Aluno salvo com sucesso", Toast.LENGTH_SHORT).show();
                 finish();
                 break;
         }
