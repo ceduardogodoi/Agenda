4f3b213cda0087420c5dadaaf1ebda56f994bca1
ceduardogodoi <carloseduardoalvesgodoi@hotmail.com>
2018-05-24 13:43:34 -0300

    Aula 04 Exercício 04: Buscando aluno no banco

--- app/src/main/java/br/com/alura/agenda/ListaAlunosActivity.java
+++ app/src/main/java/br/com/alura/agenda/ListaAlunosActivity.java
@@ -7,6 +7,11 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 
+import java.util.List;
+
+import br.com.alura.agenda.dao.AlunoDAO;
+import br.com.alura.agenda.modelo.Aluno;
+
 public class ListaAlunosActivity extends AppCompatActivity {
 
     @Override
@@ -14,12 +19,15 @@ public class ListaAlunosActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_alunos);
 
-        String[] alunos = {"Daniel", "Ronaldo", "Jeferson", "Felipe", "Ronaldo", "Jeferson",
-                "Felipe", "Ronaldo", "Jeferson", "Felipe", "Ronaldo", "Jeferson", "Felipe"};
+        AlunoDAO dao = new AlunoDAO(this);
+        List<Aluno> alunos = dao.buscaAlunos();
+        dao.close();
 
         ListView listaAlunos = findViewById(R.id.lista_alunos);
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, alunos);
+        ArrayAdapter<Aluno> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, alunos);
         listaAlunos.setAdapter(adapter);
+
+        // Vídeo parou em 11:00
 
         Button novoAluno = findViewById(R.id.novo_aluno);
         novoAluno.setOnClickListener(view -> {
