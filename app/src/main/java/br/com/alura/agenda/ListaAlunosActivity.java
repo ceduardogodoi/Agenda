559a3667a4191d2e76376962e5dfc7dc8ae99047
ceduardogodoi <carloseduardoalvesgodoi@hotmail.com>
2018-05-28 06:51:51 -0300

    Aula 05 Exercício 01: Removendo Alunos (parte 1)

--- app/src/main/java/br/com/alura/agenda/ListaAlunosActivity.java
+++ app/src/main/java/br/com/alura/agenda/ListaAlunosActivity.java
@@ -3,9 +3,14 @@ package br.com.alura.agenda;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.ContextMenu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.List;
 
@@ -14,16 +19,22 @@ import br.com.alura.agenda.modelo.Aluno;
 
 public class ListaAlunosActivity extends AppCompatActivity {
 
+    private ListView listaAlunos;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_alunos);
+
+        listaAlunos = findViewById(R.id.lista_alunos);
 
         Button novoAluno = findViewById(R.id.novo_aluno);
         novoAluno.setOnClickListener(view -> {
             Intent intentVaiProFormulario = new Intent(ListaAlunosActivity.this, FormularioActivity.class);
             startActivity(intentVaiProFormulario);
         });
+
+        registerForContextMenu(listaAlunos);
     }
 
     @Override
@@ -32,12 +43,27 @@ public class ListaAlunosActivity extends AppCompatActivity {
         carregaLista();
     }
 
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        MenuItem deletar = menu.add("Deletar");
+        deletar.setOnMenuItemClickListener(item -> {
+            AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) menuInfo;
+
+            Aluno aluno = (Aluno) listaAlunos.getItemAtPosition(info.position);
+
+            AlunoDAO dao = new AlunoDAO(ListaAlunosActivity.this);
+            dao.deleta(aluno);
+            dao.close();
+
+            return false;
+        });
+    }
+
     private void carregaLista() {
         AlunoDAO dao = new AlunoDAO(this);
         List<Aluno> alunos = dao.buscaAlunos();
         dao.close();
 
-        ListView listaAlunos = findViewById(R.id.lista_alunos);
         ArrayAdapter<Aluno> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, alunos);
         listaAlunos.setAdapter(adapter);
     }
