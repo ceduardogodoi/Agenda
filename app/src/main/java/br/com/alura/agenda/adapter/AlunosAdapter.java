32871231dc32772462af7c009a4ee4a378ec5562
ceduardogodoi <carloseduardoalvesgodoi@hotmail.com>
2018-06-26 07:10:18 -0300

    Aula 03 Atividade 03: Melhorias no layout

--- app/src/main/java/br/com/alura/agenda/adapter/AlunosAdapter.java
+++ app/src/main/java/br/com/alura/agenda/adapter/AlunosAdapter.java
@@ -1,13 +1,18 @@
 package br.com.alura.agenda.adapter;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.List;
 
+import br.com.alura.agenda.R;
 import br.com.alura.agenda.modelo.Aluno;
 
 public class AlunosAdapter extends BaseAdapter {
@@ -37,9 +42,28 @@ public class AlunosAdapter extends BaseAdapter {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        TextView view = new TextView(context);
         Aluno aluno = alunos.get(position);
-        view.setText(aluno.toString());
+
+        LayoutInflater inflater = LayoutInflater.from(context);
+
+        View view = convertView;
+        if (convertView == null) {
+            view = inflater.inflate(R.layout.list_item, parent, false);
+        }
+
+        TextView campoNome = view.findViewById(R.id.item_nome);
+        campoNome.setText(aluno.getNome());
+
+        TextView campoTelefone = view.findViewById(R.id.item_telefone);
+        campoTelefone.setText(aluno.getTelefone());
+
+        ImageView campoFoto = view.findViewById(R.id.item_foto);
+        if (aluno.getCaminhoFoto() != null) {
+            Bitmap bitmap = BitmapFactory.decodeFile(aluno.getCaminhoFoto());
+            Bitmap bitmapReduzido = Bitmap.createScaledBitmap(bitmap, 100, 100, true);
+            campoFoto.setImageBitmap(bitmapReduzido);
+            campoFoto.setScaleType(ImageView.ScaleType.FIT_XY);
+        }
 
         return view;
     }
